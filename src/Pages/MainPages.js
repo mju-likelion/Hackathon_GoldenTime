9460d318169d8bdc09a39413738cd378aaba1dc2
harksu <harksulim@naver.com>
2022-07-23 21:04:49 +0900

    Refactor : data distributed

--- src/Pages/MainPages.js
+++ src/Pages/MainPages.js
@@ -4,47 +4,10 @@ import { AiFillAlert } from "react-icons/ai";
 import DropBox from "../Components/DropBox";
 import Title from "../Components/Title";
 import Info from "../Components/Info";
-import TestLocation from "./../Components/TestLocation"; //이거 일단 주석
+import TestLocation from "../Components/TestLocation";
+import { si, seoul, gangnam, Symptom } from "../Datas/locationData.js";
 import "../Styles/AiFillAlert.scss";
 import "../Styles/DropBoxWrapper.scss";
-
-const si = [{ value: "시를 골라주세요" }, { value: "서울특별시" }]; // 데이터들 일단 여기다가 놓는데 나중에 따로 뺄겁니당
-
-const seoul = [{ value: "구를 골라주세요" }, { value: "강남구" }];
-
-const gangnam = [
-  { value: "동을 골라주세요" },
-  { value: "신사동" },
-  { value: "논현제1동" },
-  { value: "논현제2동" },
-  { value: "압구정동" },
-  { value: "청담동" },
-  { value: "삼성제1동" },
-  { value: "삼성제2동" },
-  { value: "대치제1동" },
-  { value: "대치제2동" },
-  { value: "대치제4동" },
-  { value: "역삼제1동" },
-  { value: "역삼제2동" },
-  { value: "도곡제1동" },
-  { value: "도곡제2동" },
-  { value: "개포제1동" },
-  { value: "개포제2동" },
-  { value: "개포제4동" },
-  { value: "일원본동" },
-  { value: "일원제1동" },
-  { value: "일원제2동" },
-  { value: "수서동" },
-  { value: "세곡동" },
-];
-
-const Symptom = [
-  { value: "증상을 골라주세요" },
-  { value: "천식" },
-  { value: "신체절단" },
-  { value: "화상" },
-  { value: "기타 등등" },
-];
 
 const MainPages = () => {
   const [next, setNext] = useState([]);
