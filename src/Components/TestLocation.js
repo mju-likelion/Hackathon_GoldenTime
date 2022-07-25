75d74a2a07b8bec4a33345162b5ad15e9fe05ebd
harksu <harksulim@naver.com>
2022-07-25 18:32:26 +0900

    Test : marker event

--- src/Components/TestLocation.js
+++ src/Components/TestLocation.js
@@ -9,7 +9,7 @@ const TestLocation = () => {
   const [myposx, setMyposx] = useState(0);
   const [myposy, setMyposy] = useState(0);
   //mypos는 사용자 좌표를 받는건데, 이것도 마찬가지로 y,x 순서로 렌더링 되어야함
-  useEffect(() => {
+  useEffect(() => { //이거 근데 나중에 콜백으로 바꿔야하는거 아닌가? 
     function getLocation() {
       if (navigator.geolocation) {
         // GPS를 지원하면
@@ -64,9 +64,17 @@ const TestLocation = () => {
       position: markerPosition,
     });
     marker.setMap(map);
-
-    kakao.maps.event.addListener(marker, "click", function () {
+    const iwContent = `<div style="width: 30px">${myposx} ${myposy}</div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwRemoveable = true;
+    const infowindow = new kakao.maps.InfoWindow({
+      content : iwContent,
+      removable : iwRemoveable
+  });
+    kakao.maps.event.addListener(marker, "click", function (e) {
+      
+      infowindow.open(map, marker);  
       // 마커 위에 이벤트 추가
+      console.log(myposy,myposx); //위 경도 프랍스 서버로 넘기는거 테스트 
       console.log("클릭이 되었어염"); //이거 나중에 서버에서 데이터 받을 때 상세페이지로 넘어가는 역할 (프론트에선 일단 라우팅만 하면될듯)
     });
   }); //이거 빈배열로 하면 렌더링 할 때만 이뤄져서 이건 나중에 수정
