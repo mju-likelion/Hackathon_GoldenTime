66770aac741bfee1593d1738eb646c3513c993ae
harksu <harksulim@naver.com>
2022-07-26 00:40:15 +0900

    Refactor : code refactoring

--- src/Components/TestLocation.js
+++ src/Components/TestLocation.js
@@ -1,25 +1,29 @@
 /*global kakao*/
-
 import React, { useEffect, useState, useCallback } from "react";
 import "../Styles/TestLocation.scss";
 
 const TestLocation = () => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
+
   const [myposx, setMyposx] = useState(0);
   const [myposy, setMyposy] = useState(0);
-  let polylinePath = [
-    new kakao.maps.LatLng(myposy, myposx),
-    new kakao.maps.LatLng(37.23289337377647, 127.20432065835702),
-    new kakao.maps.LatLng(37.23319495312438, 127.21033552762782),
-    new kakao.maps.LatLng(37.232166110650624, 127.21145067675457),
+
+  const polylinePath = [
+    new kakao.maps.LatLng(37.225337463214764, 127.18564851207637), //첫 시작은 사용자 위치로
+    new kakao.maps.LatLng(37.225118408434966, 127.18598957902225),
+    new kakao.maps.LatLng(37.22503190264724, 127.18782727504029),
+    new kakao.maps.LatLng(37.22544746278464, 127.18798057870085),
+    new kakao.maps.LatLng(37.22707010410253, 127.21073746069142),
+    new kakao.maps.LatLng(37.23212465363484, 127.20928734953876),
     new kakao.maps.LatLng(37.23215274661114, 127.21079718013738), //이거 중간을 네비게이션을 통해서 계속 리스트로 추가해야됨
-    new kakao.maps.LatLng(y, x),
-  ];
+    new kakao.maps.LatLng(y, x), // 마지막은 응급실 위치로
+  ]; //이거 나중에 데이터 받을 때 리스트로 보관
+
   //mypos는 사용자 좌표를 받는건데, 이것도 마찬가지로 y,x 순서로 렌더링 되어야함
+
   useEffect(() => {
-    //이거 근데 나중에 콜백으로 바꿔야하는거 아닌가?
-    function getLocation() {
+    const getLocation = () => {
       if (navigator.geolocation) {
         // GPS를 지원하면
         navigator.geolocation.getCurrentPosition(
@@ -39,7 +43,7 @@ const TestLocation = () => {
       } else {
         alert("GPS를 지원하지 않습니다");
       }
-    }
+    };
     getLocation();
   });
 
@@ -54,88 +58,62 @@ const TestLocation = () => {
     };
 
     geocoder.addressSearch(
-      "경기도 용인시 처인구 백옥대로1082번길 18", //여기는 서버에서 받은 병원의 좌표를 받아야됨
+      "경기도 용인시 처인구 백옥대로1082번길 18",
+      //여기는 서버에서 받은 병원의 좌표를 받아야됨
       getCoordinate
     );
+
     const container = document.getElementById("map");
-    console.log(x); // 디버깅용
-    console.log(y); // 디버깅용
+
     const options = {
-      center: new kakao.maps.LatLng(myposy, myposx),
-      level: 3,
+      center: new kakao.maps.LatLng(37.225337463214764, 127.18564851207637), //이것도 나중에 gps 값으로
+      level: 5,
     };
 
     const map = new kakao.maps.Map(container, options);
 
-    var positions = [
+    const positions = [
+      //이것도 나중엔 데이터 받아서 리스트로 관리
       {
-        title: "카카오",
-        latlng: new kakao.maps.LatLng(myposy, myposx),
+        latlng: new kakao.maps.LatLng(37.225337463214764, 127.18564851207637),
       },
       {
-        title: "생태연못",
         latlng: new kakao.maps.LatLng(y, x),
       },
     ];
-    var imageSrc =
-      "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
-    for (var i = 0; i < positions.length; i++) {
-      // 마커 이미지의 이미지 크기 입니다
-      var imageSize = new kakao.maps.Size(24, 35);
-
-      // 마커 이미지를 생성합니다
-      //var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-      // 마커를 생성합니다
-      var marker = new kakao.maps.Marker({
+    for (let i = 0; i < positions.length; i++) {
+      let marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: positions[i].latlng, // 마커를 표시할 위치
-        title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        //image: markerImage, // 마커 이미지
+        title: positions[i].title,
+      });
+      kakao.maps.event.addListener(marker, "click", function () {
+        infowindow.open(map, marker);
+        console.log(myposy, myposx); //위 경도 프랍스 서버로 넘기는거 테스트
+        //상세페이지로 넘어가는 역할
       });
     }
 
-    // const markerPosition = new kakao.maps.LatLng(myposy, myposx);
-    // const markerPosition1 = new kakao.maps.LatLng(y, x);
-    // const marker = new kakao.maps.Marker({
-    //   position: markerPosition,
-    // });
-    // const destmarker = new kakao.maps.Marker({
-    //   position: markerPosition1,
-    // });
-
-    //marker.setMap(map);
-    // destmarker.setMap(map);
-
-    const iwContent = `<div style="width: 30px">${myposx} ${myposy}</div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    const iwContent = `<div style="width: 30px">${myposx} ${myposy}</div>`,
+      // 인포윈도우에 표출될 내용
       iwRemoveable = true;
+
     const infowindow = new kakao.maps.InfoWindow({
       content: iwContent,
       removable: iwRemoveable,
-    });
-    kakao.maps.event.addListener(marker, "click", function (e) {
-      infowindow.open(map, marker);
-      // 마커 위에 이벤트 추가
-      console.log(myposy, myposx); //위 경도 프랍스 서버로 넘기는거 테스트
-      console.log("클릭이 되었어염"); //이거 나중에 서버에서 데이터 받을 때 상세페이지로 넘어가는 역할 (프론트에선 일단 라우팅만 하면될듯)
     });
 
     const polyline = new kakao.maps.Polyline({
       path: polylinePath, //좌표배열
       strokeColor: "#FF0000", //선의 색 빨강
       strokeOpacity: 0.8, //선의 투명도
-      strokeWeight: 6, //선의 두께
+      strokeWeight: 3, //선의 두께
       map: map, //만들어 놓은 지도
     });
     polyline.setMap(map);
-  }, []); //이거 빈배열로 하면 렌더링 할 때만 이뤄져서 이건 나중에 수정
+  });
 
-  return (
-    <>
-      <div id="map" className="TestLocation"></div>
-      <div id="mapp"></div>
-    </>
-  );
+  return <div id="map" className="TestLocation"></div>;
 };
 export default TestLocation;
