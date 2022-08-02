222ad22a87fcab9a444e398e758bab1d681db806
harksu <harksulim@naver.com>
2022-08-03 02:03:25 +0900

    Refactor : edit phone call event using recoil

--- src/Components/DetailInfo.js
+++ src/Components/DetailInfo.js
@@ -2,26 +2,32 @@ import React from "react";
 import { useRecoilValue } from "recoil";
 import "../Styles/Info.scss";
 import image from "../Datas/image.jpg";
-import { details } from "../Atoms/atoms";
+import { details, infoData } from "../Atoms/atoms";
 
 const DetailInfo = () => {
   const detailsValue = useRecoilValue(details);
+  const infoValue = useRecoilValue(infoData);
   const { distance, time } = detailsValue;
+  const { address, title, callNumber } = infoValue;
+  const km = parseInt(distance / 1000);
+  const m = distance % 1000;
   const minute = parseInt(time / 60);
   const second = time % 60;
+  const phoneCall = () => {
+    window.location.href = "tel:" + callNumber;
+  };
 
-  console.log(detailsValue);
   return (
     <div className="Info">
       <img className="hospitalImage" alt="hospitalImage" src={image} />
       <div className="InfoList">
-        <p className="InfoText">xxx응급실</p>
-        <p className="InfoText">증상 진료 여부 확인 : y</p>
-        <p className="InfoText">입원 가능 여부 : y</p>
-        <p className="InfoText">상세 위치 : 경기도 용인시 처인구 남동 ~~~~</p>
-        <p className="InfoText">Tel : 010 -xxxx -xxxx </p>
+        <p className="InfoText">{address}</p>
+        <p className="InfoText">{title}</p>
+        <p className="InfoText" onClick={phoneCall}>
+          Tel : {callNumber}
+        </p>
         <p className="DetailInfoText">
-          최단 거리는 {distance} m이며, 예상 소요시간은 {minute}분 {second}초
+          최단 거리는 {km}.{m} km이며, 예상 소요시간은 {minute}분 {second}초
           입니다.
         </p>
       </div>
