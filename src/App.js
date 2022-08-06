51585c69859fcad185fe1b2ec91370b6b350edf3
harksu <harksulim@naver.com>
2022-08-07 03:54:55 +0900

    Feat : page swap animation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import DetailPage from "./Pages/DetailPage";
 import FirstAidPage from "./Pages/FirstAidPage";
 import ListPage from "./Pages/ListPage";
 import MainPages from "./Pages/MainPages";
 
 function App() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<MainPages />} />
-      <Route path="/detail" element={<DetailPage />} />
-      <Route path="/aid" element={<FirstAidPage />} />
-      <Route path="/list" element={<ListPage />} />
-    </Routes>
+    <TransitionGroup className="transitions-wrapper">
+      <CSSTransition key={location.pathname} classNames={"right"} timeout={300}>
+        <Routes>
+          <Route path="/" element={<MainPages />} />
+          <Route path="/detail" element={<DetailPage />} />
+          <Route path="/aid" element={<FirstAidPage />} />
+          <Route path="/list" element={<ListPage />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
   );
 }
 
