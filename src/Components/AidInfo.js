5b82ecf53c914b55019c3d8302e00f435524a30f
harksu <harksulim@naver.com>
2022-08-14 17:46:31 +0900

    Style : delete inner scroll

--- src/Components/AidInfo.js
+++ src/Components/AidInfo.js
@@ -6,15 +6,17 @@ import "../Styles/Info.scss";
 const AidInfo = () => {
   const recoilValue = useRecoilValue(aidInfos);
 
-  const { firstAid,firstImage } = recoilValue;
+  const { firstAid, firstImage } = recoilValue;
 
-  const firstaidcommma  = firstAid.split(".");
-  const firstAidNew = firstaidcommma.map ( (firstaidcommmas,index) => <li key = {index}>{firstaidcommmas}</li>);    // 점 찍힌 곳마다 줄 바꿈
+  const firstaidcommma = firstAid.split(".");
+  const firstAidNew = firstaidcommma.map((firstaidcommmas, index) => (
+    <li key={index}>{firstaidcommmas}</li>
+  )); // 점 찍힌 곳마다 줄 바꿈
 
   return (
     <div className="aidInfo">
-       <img className="hospitalImage"  alt="AidInfoImage" src={firstImage}></img> 
-       <div className="InfoList">
+      <img className="hospitalImage" alt="AidInfoImage" src={firstImage}></img>
+      <div className="InfoList">
         <p className="InfoText">{firstAidNew}</p>
       </div>
     </div>
